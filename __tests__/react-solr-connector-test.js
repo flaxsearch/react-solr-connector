f3ea146d4c3bdea44d46701588841c71a97eddd5
Tom Mortimer <tom@husky.lan>
2016-06-27 14:17:17 +0100

    started to add jest tests

---
+++ __tests__/react-solr-connector-test.js
@@ -0,0 +1,11 @@
+jest.disableAutomock();
+
+import React from 'react';
+import SolrConnector from '../src/react-solr-connector';
+
+describe('react-solr-connecto', () => {
+  it('FIXME', () => {
+    const sc = <SolrConnector/>;
+    expect(sc).toBeDefined();
+  });
+})
